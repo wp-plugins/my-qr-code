62ec4a490d300ec160b3254adf3b77ffc1c7b4ac
mrdimeh <mrdimeh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-24 13:35:45 +0000

    Upgrade New WIFI Network for Android
    git-svn-id: https://plugins.svn.wordpress.org/my-qr-code/trunk@644080 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,24 +2,29 @@
 /*
 Plugin Name: My QR Code Plugin
 Plugin URI: http://www.eksit.co/qrcode
-Description: This plugin allows you to insert a QR Tag into any WordPress post or page, there are a number of different options that can be encoded into the QR Tag to make the user do as you want, for instance you can make the user:
+Description: You can use these in your sales pitch to your "Local Offline businesses", you can add the QR Code tag to ANY NEW or EXISTING post or page by clicking the 'Add Tag' widget to insert the short code that displays the 'QR Tag' where you want it to be displayed in your post, click to publish or update the post to finish the procedure.
+My QR Code allows you to insert a QR Tag into any WordPress post or page, there are a number of different options that can be encoded into the QR Tag to make the user do as you want..., for instance you can make the user.
 
-Browse to a Website
-Bookmark a Website
-Make a Phone Call
-Send an SMS
-Send an E-Mail
-Create a vCard
-Create a meCard
-Create a vCalendar Event
-Youtube URL for iPhone
-Tweet on Twitter
-Create Blackberry Messenger User
-WIFI Network for Android
-Free Formatted Text
+**Current add-ons**
+
+* Browse to a Website
+* Bookmark a Website
+* Make a Phone Call
+* Send an SMS
+* Send an E-Mail
+* Create a vCard
+* Create a meCard
+* Create a vCalendar Event
+* Youtube URL for iPhone
+* Tweet on Twitter
+* Create Blackberry Messenger User
+* WIFI Network for Android
+* Free Formatted Text
+
+If you have suggestions for a new add-on, feel free to email me at mrdimeh@eksit.co. Want regular updates? Follow me on Twitter!  http://twitter.com/mrdimeh
 
 Author: Mehdi Laidouni  
-Version: 1.0
+Version: 1.5
 Author URI: http://www.eksit.co
 License: GPL2
 */
