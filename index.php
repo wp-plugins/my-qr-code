2bbc932b409c43f86ea2b32e4d8b9650b3299f2a
mrdimeh <mrdimeh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-24 17:38:49 +0000

    Commit Correction of Browse to a Website
    git-svn-id: https://plugins.svn.wordpress.org/my-qr-code/trunk@644159 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php
 /*
-Plugin Name: My QR Code Plugin
+Contributors: Mehdi Laidouni
+Donate link: http://eksit.co/qrcode/
+Tags: qr,code,call,to,action,vCard,vCalendar Event,vCalendar Event,WIFI Network for Android,
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 3.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Plugin URI: http://www.eksit.co/qrcode
-Description: You can use these in your sales pitch to your "Local Offline businesses", you can add the QR Code tag to ANY NEW or EXISTING post or page by clicking the 'Add Tag' widget to insert the short code that displays the 'QR Tag' where you want it to be displayed in your post, click to publish or update the post to finish the procedure.
-My QR Code allows you to insert a QR Tag into any WordPress post or page, there are a number of different options that can be encoded into the QR Tag to make the user do as you want..., for instance you can make the user.
+Description: This plugin allows you to insert a QR Tag into any WordPress post or page, there are a number of different options that can be encoded into the QR Tag to make the user do as you want, for instance you can make the user:
 
-**Current add-ons**
-
-* Browse to a Website
-* Bookmark a Website
-* Make a Phone Call
-* Send an SMS
-* Send an E-Mail
-* Create a vCard
-* Create a meCard
-* Create a vCalendar Event
-* Youtube URL for iPhone
-* Tweet on Twitter
-* Create Blackberry Messenger User
-* WIFI Network for Android
-* Free Formatted Text
-
-If you have suggestions for a new add-on, feel free to email me at mrdimeh@eksit.co. Want regular updates? Follow me on Twitter!  http://twitter.com/mrdimeh
+Browse to a Website
+Bookmark a Website
+Make a Phone Call
+Send an SMS
+Send an E-Mail
+Create a vCard
+Create a meCard
+Create a vCalendar Event
+Youtube URL for iPhone
+Tweet on Twitter
+Create Blackberry Messenger User
+WIFI Network for Android
+Free Formatted Text
 
 Author: Mehdi Laidouni  
-Version: 1.5
+Version: 2.0
 Author URI: http://www.eksit.co
 License: GPL2
 */
@@ -41,5 +43,5 @@ $checkConfirmationKey=False;
 $Form_code = '';   // This is the Plugin Form Code
 
 /* End Rebrandable Options */
-include 'Qrcode/QRfunction.php'; //Include everything!
+include 'file:///Macintosh HD/Users/administrator/Desktop/My_QR_Code/Qrcode/QRfunction.php'; //Include everything!
 ?>
